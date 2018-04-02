c9209047f2491502b0208829d20c39677d5fd65d
裘烈 <qiulie@qccr.com>
2018-04-02 15:33:41 +0800

    Initial commit

---
+++ app/src/main/java/com/ql0571/constraintlayoutdemo/MainActivity.java
@@ -0,0 +1,19 @@
+package com.ql0571.constraintlayoutdemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onDemoClick(View v) {
+        startActivity(new Intent(this, DemoActivity.class));
+    }
+}
